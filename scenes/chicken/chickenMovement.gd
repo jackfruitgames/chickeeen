1c47537226154218e51917ca8cacc21cbcff24d7
eddex <eddex@outlook.com>
2023-11-03 23:48:02 +0100

    🏗: le big refactor

--- scenes/chicken/chickenMovement.gd
+++ scenes/chicken/chickenMovement.gd
@@ -5,9 +5,9 @@ const SPEED = 25.0
 func _physics_process(delta):
 	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = input_direction * SPEED
-	if input_direction != Vector2(0, 0) and !$WalkAudioStreamPlayer2D.playing:
-		$WalkAudioStreamPlayer2D.play()
-	
-	$Sprite2D.flip_h = input_direction.x < 0
+	if input_direction != Vector2(0, 0):
+		if !$WalkAudioStreamPlayer2D.playing:
+			$WalkAudioStreamPlayer2D.play()
+		$Sprite2D.flip_h = input_direction.x < 0
 
 	move_and_slide()
