51f49211972c7e71434f44d6a2635bd86428bd3c
eddex <eddex@outlook.com>
2023-11-03 22:52:17 +0100

    🎶🐔: chickeeen movement sounds!

--- scenes/chicken/chickenMovement.gd
+++ scenes/chicken/chickenMovement.gd
@@ -5,5 +5,7 @@ const SPEED = 25.0
 func _physics_process(delta):
 	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = input_direction * SPEED
+	if input_direction != Vector2(0, 0) and !$WalkAudioStreamPlayer2D.playing:
+		$WalkAudioStreamPlayer2D.play()
 
 	move_and_slide()
