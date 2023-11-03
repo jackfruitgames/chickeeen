6449a2d74e60359588fdbf50a5ecc36bc012c16a
Jan Bucher <jan@bucher.cloud>
2023-11-03 19:31:51 +0100

    Make the chicken move

---
+++ scenes/chicken/chickenMovement.gd
@@ -0,0 +1,9 @@
+extends CharacterBody2D
+
+const SPEED = 300.0
+
+func _physics_process(delta):
+	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	velocity = input_direction * SPEED
+
+	move_and_slide()
