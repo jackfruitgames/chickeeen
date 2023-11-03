69187b7b1bcd621cb16ad58599e9908911d6908f
Benj Fassbind <randombenj@gmail.com>
2023-11-03 22:02:07 +0100

    Chicken is not big

--- scenes/chicken/chickenMovement.gd
+++ scenes/chicken/chickenMovement.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
+const SPEED = 25.0
 
 func _physics_process(delta):
 	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
