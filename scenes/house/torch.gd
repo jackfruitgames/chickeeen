064b4888ade980b843a304148294ed642a6399ae
Benj Fassbind <randombenj@gmail.com>
2023-11-04 17:07:31 +0100

    Beeing at home is the chicken livestyle

---
+++ scenes/house/torch.gd
@@ -0,0 +1,12 @@
+extends PointLight2D
+
+@onready var noise = FastNoiseLite.new()
+var rng = RandomNumberGenerator.new()
+
+var time_passed = rng.randf_range(0.0, 10.0)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	time_passed += delta
+	var flicker_noise = abs(noise.get_noise_1d(time_passed * 10)) * 2 + 1
+	energy = flicker_noise
