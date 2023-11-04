3f43ae1044cadeb9d17e29f0cec1278bfef0c757
Jan Bucher <jan@bucher.cloud>
2023-11-04 15:46:07 +0100

    fix food to only be eaten if collided with player
    Otherwise it will just be destroyed to avoid strange looking food inside
    other elements.

--- scenes/food/eating.gd
+++ scenes/food/eating.gd
@@ -1,7 +1,10 @@
 extends Area2D
 
-func _on_body_entered(body):
-	GameState.score += 10
-	print("score: " + str(GameState.score))
-	$EatAudioStreamPlayer2D.play()
+func _on_body_entered(collided_object):
+	if collided_object.is_in_group("player"):
+		GameState.score += 10
+		print("score: " + str(GameState.score))
+		$EatAudioStreamPlayer2D.play()
+
+	# destroy food if it is spawned random inside a tree or some other object
 	queue_free()
