b58cd2906b7e043acc67f7b331afe345953cae93
Jan Bucher <jan@bucher.cloud>
2023-11-05 19:50:51 +0100

    make shotgun work
    Co-Authored-by: Benj Fassbind <randombenj@gmail.com>

--- scenes/chicken/shotgun.gd
+++ scenes/chicken/shotgun.gd
@@ -2,28 +2,51 @@ extends Node2D
 
 var shoot_wolfs = []
 
-var RAYS := [
-	$ShotsFIred/ShotgunRayCast,
-	$ShotsFIred/ShotgunRayCast2,
-	$ShotsFIred/ShotgunRayCast3,
-	$ShotsFIred/ShotgunRayCast4,
-	$ShotsFIred/ShotgunRayCast5,
-]
-
 
 func _physics_process(delta: float):
+	if $ShotsFIred/ShotgunGPUParticles2D.emitting:
+		return
+
 	var target_point = global_position + get_parent().velocity
 	$ShotsFIred.look_at(target_point)
 	if Input.is_action_just_pressed("shoot"):
 		$ShotsFIred/ShotgunGPUParticles2D.emitting = true
+		$ShotsFIred/ShotgunRayCast
 
-		for ray in RAYS:
-			if ray.is_colliding():
-				var collider = ray.get_collider()
-				if collider != null and collider.is_in_group("wolf"):
-					print("killed a wolf")
-					shoot_wolfs.append(collider)
-					$WolfKillerTimer.start()
+		if $ShotsFIred/ShotgunRayCast.is_colliding():
+			var collider = $ShotsFIred/ShotgunRayCast.get_collider()
+			if collider != null and collider.is_in_group("wolf"):
+				print("killed a wolf")
+				shoot_wolfs.append(collider)
+				$WolfKillerTimer.start()
+
+		if $ShotsFIred/ShotgunRayCast2.is_colliding():
+			var collider = $ShotsFIred/ShotgunRayCast2.get_collider()
+			if collider != null and collider.is_in_group("wolf"):
+				print("killed a wolf")
+				shoot_wolfs.append(collider)
+				$WolfKillerTimer.start()
+
+		if $ShotsFIred/ShotgunRayCast3.is_colliding():
+			var collider = $ShotsFIred/ShotgunRayCast3.get_collider()
+			if collider != null and collider.is_in_group("wolf"):
+				print("killed a wolf")
+				shoot_wolfs.append(collider)
+				$WolfKillerTimer.start()
+
+		if $ShotsFIred/ShotgunRayCast4.is_colliding():
+			var collider = $ShotsFIred/ShotgunRayCast4.get_collider()
+			if collider != null and collider.is_in_group("wolf"):
+				print("killed a wolf")
+				shoot_wolfs.append(collider)
+				$WolfKillerTimer.start()
+
+		if $ShotsFIred/ShotgunRayCast5.is_colliding():
+			var collider = $ShotsFIred/ShotgunRayCast5.get_collider()
+			if collider != null and collider.is_in_group("wolf"):
+				print("killed a wolf")
+				shoot_wolfs.append(collider)
+				$WolfKillerTimer.start()
 
 func _on_wolf_killer_timer_timeout():
 	for w in shoot_wolfs:
