4d2953eddb03e2ab2e50d558a9647010527eac0b
Benj Fassbind <randombenj@gmail.com>
2023-11-05 12:23:48 +0100

    Add warning color to progressbar

--- scenes/ui/UpdateFoodProgress.gd
+++ scenes/ui/UpdateFoodProgress.gd
@@ -2,3 +2,12 @@ extends ProgressBar
 
 func _process(delta):
 	value = GameState.score
+	
+	if value <= 20:
+		var sb = StyleBoxFlat.new()
+		add_theme_stylebox_override("fill", sb)
+		sb.bg_color = Color("ff0000")
+	else:
+		var sb = StyleBoxFlat.new()
+		add_theme_stylebox_override("fill", sb)
+		sb.bg_color = Color("ffffff")
