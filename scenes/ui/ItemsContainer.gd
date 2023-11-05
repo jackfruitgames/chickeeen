5622a925094ba3523fd5400a5dcd29b0c7202c5b
Jan Bucher <jan@bucher.cloud>
2023-11-05 21:33:33 +0100

    Question what is going on we don't know or do we ❓
    Co-Authored-By: Benj Fassbind <randombenj@gmail.com>

--- scenes/ui/ItemsContainer.gd
+++ scenes/ui/ItemsContainer.gd
@@ -3,10 +3,13 @@ extends HBoxContainer
 func _process(_delta:float):
 	if GameState.has_swimmer:
 		$Swimmer.visible = true
+		$SwimmerP.visible = false
 
 	if GameState.has_torch:
 		$Lantern.visible = true
+		$LanternP.visible = false
 
 	if GameState.has_shotgun:
 		$Shotgun.visible = true
 		$AmmoContainer.visible = true
+		$ShotgunP.visible = false
