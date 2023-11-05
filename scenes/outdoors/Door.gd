21d916ea1cff41aac4eb08fae76d4dec68042cc7
Jan Bucher <jan@bucher.cloud>
2023-11-05 15:57:41 +0100

    only collide the door with the player 💥

--- scenes/outdoors/Door.gd
+++ scenes/outdoors/Door.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
 
-func _on_body_entered(body):
-	get_tree().change_scene_to_file("res://scenes/house/house.tscn")
+func _on_body_entered(collided_object):
+	if collided_object.is_in_group("player"):
+		get_tree().change_scene_to_file("res://scenes/house/house.tscn")
