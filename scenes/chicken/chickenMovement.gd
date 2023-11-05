125e664c23b1485e1b5527d39ae2510edfa17664
Jan Bucher <jan@bucher.cloud>
2023-11-05 14:36:26 +0100

    prevent error in chicken movement if tilemap cannot be found

--- scenes/chicken/chickenMovement.gd
+++ scenes/chicken/chickenMovement.gd
@@ -7,19 +7,20 @@ const WATER_TILE_TERRAIN := 2
 const SWIMMING = preload("res://assets/chicken/Chicken-half.png")
 const NORMAL = preload("res://assets/chicken/Chicken-0001.png")
 
+var map: TileMap = null
 
 func _ready():
+	map = get_tree().get_root().get_node_or_null("/root/outdoors/Map")
 	if get_parent().name == "house":
 		speed = 50
 
 func _process(delta):
-	var map: TileMap = $%Map
-	if !map:
+	if not map:
 		return
-		
+
 	var tile := map.local_to_map(get_position())
 	var data = map.get_cell_tile_data(0, tile)
-	
+
 	if data.terrain == WATER_TILE_TERRAIN && GameState.has_swimmer:
 		# chicken is in water
 		GameState.is_in_water = true
@@ -30,7 +31,6 @@ func _process(delta):
 		$Sprite2D/Swimmer.visible = false
 		$Sprite2D.texture = NORMAL
 
-	
 func _physics_process(delta):
 	var real_speed = speed * clamp(GameState.level, 1, 3)
 	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
