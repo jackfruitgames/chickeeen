13b66853443733a9845e3eaa81224d23f5899fa8
eddex <eddex@outlook.com>
2023-11-05 20:50:58 +0100

    🌊: chicken can swim after entering the house and going outside again

--- scenes/outdoors/Swimmy.gd
+++ scenes/outdoors/Swimmy.gd
@@ -1,5 +1,14 @@
 extends Area2D
 
+func _ready():
+	if GameState.has_swimmer:
+		visible = false
+		
+		var body = get_parent().get_node("chicken")
+		# allow the chicken to swim (not collide with water)
+		# starting from level 5
+		body.set_collision_layer_value(4, false)
+		body.set_collision_mask_value(4, false)
 
 func _on_body_entered(body):
 	if body.is_in_group("player"):
