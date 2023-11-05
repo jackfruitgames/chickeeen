81ed1427b8ff9887d3ab1ae2eb3b198c6abda0ee
Benj Fassbind <randombenj@gmail.com>
2023-11-05 12:32:35 +0100

    House does not shine

--- scenes/house/torch.gd
+++ scenes/house/torch.gd
@@ -9,4 +9,7 @@ var time_passed = rng.randf_range(0.0, 10.0)
 func _process(delta):
 	time_passed += delta
 	var flicker_noise = abs(noise.get_noise_1d(time_passed * 10)) * 2 + 1
-	energy = flicker_noise
+	var night_progress = float(GameState.dawn_time) / float(GameState.DAWN_TIME)
+	if GameState.is_night:
+		night_progress = 0
+	energy = flicker_noise * (1 - night_progress)
